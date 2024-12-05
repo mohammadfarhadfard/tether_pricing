ede345c3f3fd05099bdbca50939537e7b314832e
MohammadFarhadfard <mohammadfarhadfard@gmail.com>
2024-12-05 16:53:41 +0330

    change some things

--- app.js
+++ app.js
@@ -1,82 +1,87 @@
-const axios = require('axios');
-const readline = require('readline');
-require('dotenv').config()
-let NOBITEX = process.env.NOBITEX_Route
+const axios = require("axios");
+const readline = require("readline");
+require("dotenv").config();
+let NOBITEX = process.env.NOBITEX_Route;
 // let WALLEX = process.env.WALLEX_Route
 
 //reading data from console
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 //function to get the order book
 async function getOrderBook(action, amountRequested) {
-    try {
-        const response = await axios.get(NOBITEX);
-        
-        const buy = response.data.bids; 
-        const sell = response.data.asks;  
+  try {
+    const response = await axios.get(NOBITEX);
 
-        let totalPrice = 0;
-        let totalAmount = 0;
+    const buy = response.data.bids;
+    const sell = response.data.asks;
 
-        if (action.toLowerCase() === 'buy') {
-            //buying
-            for (let order of sell) {
-                const price = parseFloat(order[0]);
-                const availableAmount = parseFloat(order[1]);
+    let totalPrice = 0;
+    let totalAmount = 0;
 
-                if (amountRequested <= availableAmount) {
-                    totalPrice += price * amountRequested;
-                    totalAmount += amountRequested;
-                    break;
-                } else {
-                    totalPrice += price * availableAmount;
-                    totalAmount += availableAmount;
-                    amountRequested -= availableAmount;
-                }
-            }
-        } else if (action.toLowerCase() === 'sell') {
-            //selling
-            for (let order of buy) {
-                const price = parseFloat(order[0]);
-                const availableAmount = parseFloat(order[1]);
+    if (action.toLowerCase() === "buy") {
+      //buying
+      for (let order of sell) {
+        const price = parseFloat(order[0]);
+        const availableAmount = parseFloat(order[1]);
 
-                if (amountRequested <= availableAmount) {
-                    totalPrice += price * amountRequested;
-                    totalAmount += amountRequested;
-                    break;
-                } else {
-                    totalPrice += price * availableAmount;
-                    totalAmount += availableAmount; 
-                    amountRequested -= availableAmount;
-                }
-            }
+        if (amountRequested <= availableAmount) {
+          totalPrice += price * amountRequested;
+          totalAmount += amountRequested;
+          break;
         } else {
-            console.log("Invalid action. Please specify 'buy' or 'sell'.");
-            return;
+          totalPrice += price * availableAmount;
+          totalAmount += availableAmount;
+          amountRequested -= availableAmount;
         }
+      }
+    } else if (action.toLowerCase() === "sell") {
+      //selling
+      for (let order of buy) {
+        const price = parseFloat(order[0]);
+        const availableAmount = parseFloat(order[1]);
 
-        //average price
-        const averagePrice = totalAmount > 0 ? totalPrice / totalAmount : 0;
-
-        console.log(`Total price for ${action}ing ${totalAmount} is: ${totalPrice}`);
-        console.log(`Average price for ${action}ing is: ${averagePrice}`);
-    } catch (error) {
-        console.error('Error fetching order book:', error);
+        if (amountRequested <= availableAmount) {
+          totalPrice += price * amountRequested;
+          totalAmount += amountRequested;
+          break;
+        } else {
+          totalPrice += price * availableAmount;
+          totalAmount += availableAmount;
+          amountRequested -= availableAmount;
+        }
+      }
+    } else {
+      console.log("Invalid action. Please specify 'buy' or 'sell'.");
+      return;
     }
+
+    //average price
+    const averagePrice = totalAmount > 0 ? totalPrice / totalAmount : 0;
+
+    console.log(
+      `Total price for ${action}ing ${totalAmount} is: ${totalPrice}`
+    );
+    console.log(`Average price for ${action}ing is: ${averagePrice}`);
+  } catch (error) {
+    console.error("Error fetching order book:", error);
+  }
 }
 
 //function to ask from user
 function askForDetails() {
-    rl.question('Do you want to buy or sell? (type "buy" or "sell"): ', (action) => {
-        rl.question('Please enter the amount you want to request: ', (amount) => {
-            getOrderBook(action, parseFloat(amount));
-            //close the readline
-            rl.close();
-        });
-    });
+  rl.question(
+    'Do you want to buy or sell? (type "buy" or "sell"): ',
+    (action) => {
+      rl.question("Please enter the amount you want to request: ", (amount) => {
+        getOrderBook(action, parseFloat(amount));
+        //close the readline
+        rl.close();
+      });
+    }
+  );
 }
 
 //start the process
